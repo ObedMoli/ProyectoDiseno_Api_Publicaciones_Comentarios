8608b0dde08a59d2fb64df06f00119882a71dcaa
Sluu05 <saubdy.ramirez@unah.hn>
2025-08-03 10:54:16 -0600

    Mis cambios locales en server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import authRoutes from './routes/auth.routes.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 dotenv.config();
 
 const app = express();
-app.use(express.json());
 
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/auth', authRoutes);
+
+// Error handling middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
