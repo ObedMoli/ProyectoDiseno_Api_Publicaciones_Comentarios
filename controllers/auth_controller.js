0472c4192e185061d9c04a5c92c21d61dd4a70b2
Sluu05 <saubdy.ramirez@unah.hn>
2025-08-03 11:20:18 -0600

    Creacion controller de usuario

---
+++ controllers/auth_controller.js
@@ -0,0 +1,80 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { v4 as uuidv4 } from 'uuid';
+import { createUser, getUserByEmail } from '../models/user_model.js';
+import { registerSchema, validateLogin } from '../schemas/validators_user.js';
+import { conflict, created, ok, unauthorized, badRequest } from '../utils/utils.js';
+
+export const register = async (req, res, next) => {
+
+    try {
+        const result = registerSchema.safeParse(req.body);
+
+        if (!result.success) {
+
+            const validationErrors = result.error?.errors || [];
+
+            return res.status(400).json(
+                badRequest('Error de validación', validationErrors.map(e =>({
+                    campo: e.path.join('.'), 
+                    mensaje: e.message
+                })))
+            );
+        }
+
+        const { name, email, password, telefono, about } = result.data;
+
+        const existingUser = await getUserByEmail(email);
+        if (existingUser) {
+            return res.status(400).json({ message: 'El correo electrónico ya está en uso' });
+        }
+
+        const password_hash = await bcrypt.hash(password, 10);
+        const user_id = Buffer.from(uuidv4().replace(/-/g, ''), 'hex');
+
+        await createUser({ user_id, name, email, password_hash, telefono, about });
+
+        res.status(201).json({ message: 'Usuario creado exitosamente' });
+
+    } catch (error) {
+        next(error);
+    }
+
+};
+
+
+export const login = async (req, res, next) => {
+
+    try {
+        const result = validateLogin.safeParse(req.body);
+
+        if (!result.success) {
+            return res.status(400).json(
+                badRequest('Error de validación', result.error.errors.map(e => ({
+                    campo: e.path.join('.'),
+                    mensaje: e.message
+                })))
+            );
+        }
+
+        const { email, password } = result.data;       
+
+        const user = await getUserByEmail(email);
+        if (!user) {
+            return res.status(401).json( unauthorized ( 'Credenciales inválidas' ));
+        }
+
+        const match = await bcrypt.compare(password, user.password_hash);
+        if (!match) {
+            return res.status(401).json( unauthorized ( 'Credenciales inválidas' ));
+        }
+
+        const token = jwt.sign({ id: user.user_id.toString('hex'), name: user.name }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.status(201).json(ok('Inicio de sesión exitoso',{ token }));
+        
+    } catch (error) {
+        next(error);
+    }
+};
+
