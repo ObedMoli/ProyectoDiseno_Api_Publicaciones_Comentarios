7477d84192840c665cae1332e7f5c35270efbf77
Jorge Reyes <jreycruz1126@gmail.com>
2025-08-05 13:51:29 -0600

    agregando rutas y endpoint de comentarios

--- routes/comentarioRoutes.js
+++ routes/comentarioRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { obtenerComentarios } from '../controllers/comentarioController.js';
+import {
+  obtenerTodosLosComentarios,
+  obtenerComentarios,
+  publicarComentario
+} from '../controllers/comentarioController.js';
+import { verifyToken } from '../middlewares/verify_token.js';
 
 const router = express.Router();
 
+router.get('/comentarios', obtenerTodosLosComentarios);
+
 router.get('/publicaciones/:id/comentarios', obtenerComentarios);
+
+router.post('/publicaciones/:id/comentarios', verifyToken, publicarComentario);
 
 export default router;
