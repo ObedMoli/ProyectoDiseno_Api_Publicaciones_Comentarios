5fab43710df4cc2e204ddc0134fcee4976b0c840
Jorge Reyes <jreycruz1126@gmail.com>
2025-08-05 13:05:59 -0600

    actualizando comentarios para agregar uno nuevo

--- models/comentarioModel.js
+++ models/comentarioModel.js
@@ -1,9 +1,24 @@
 import pool from '../config/db.js';
 
+export const getTodosLosComentarios = async () => {
+  const [rows] = await pool.query(
+    'SELECT * FROM comment ORDER BY date DESC'
+  );
+  return rows;
+};
+
 export const getComentariosByPostId = async (postId) => {
   const [rows] = await pool.query(
     'SELECT * FROM comment WHERE post_post_id = ? ORDER BY date DESC',
     [postId]
   );
   return rows;
+};
+
+export const crearComentario = async (comentario) => {
+  const { comment_by_user_name, content, post_post_id, user_user_id } = comentario;
+  await pool.query(
+    'INSERT INTO comment (comment_by_user_name, content, post_post_id, user_user_id) VALUES (?, ?, ?, ?)',
+    [comment_by_user_name, content, post_post_id, user_user_id]
+  );
 };
