6871a1471c2852543fdec28f90a85aff28a7e36b
Sluu05 <saubdy.ramirez@unah.hn>
2025-08-10 02:30:12 -0600

    arreglando confusion en validaciones de user

--- schemas/validators_user.js
+++ schemas/validators_user.js
@@ -4,7 +4,7 @@ export const registerSchema =z.object({
    
     name: z.string({required_error: 'El nombre es obligatorio' }).min(3, 'El nombre es obligatorio').max(50),
     email: z.string({required_error: 'El email es obligatorio' }).email('El correo electrónico no es válido'),
-    password: z.string({required_error: 'La contraseña es obligatoria' }).min(6, 'La contraseña debe tener al menos 6 caracteres'),
+    password: z.string({required_error: 'La contraseña es obligatoria' }).min(8, 'La contraseña debe tener al menos 8 caracteres'),
     telefono: z.string().optional(),
     about: z.string().optional(),
     role_name: z.string({required_error: 'El rol es obligatorio' })
@@ -14,7 +14,7 @@ export const registerSchema =z.object({
 
 export const validateLogin = z.object({
     email: z.string().email(),
-    password: z.string().min(6)
+    password: z.string().min(8)
 });
 
     
