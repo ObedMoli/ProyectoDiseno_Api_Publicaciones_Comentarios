61fe746d062f19e13132242ad8647052cfe0287f
ObedEsauMolinaSandoval <obedmolinas@gmail.com>
2025-08-09 23:23:37 -0600

    se agrego el puerto para el front

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'http://127.0.0.1:3308'
+    'http://127.0.0.1:3308',
+    'http://localhost:5173'
   ],
 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
